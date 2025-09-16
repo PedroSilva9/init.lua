bfd272be17465df38801921871668aca1c5c8a40
Pedro Silva <pedro.silva@dipcode.com>
2025-09-16 17:43:21 +0100

    stage commit

--- lua/luk/remap.lua
+++ lua/luk/remap.lua
@@ -19,6 +19,8 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 vim.api.nvim_create_autocmd('TextYankPost', {
